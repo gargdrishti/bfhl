0e56697c338efd25345bdce28b0a367edadf2b7f
drishtigarg <drishtigarg10@gmail.com>
2026-06-24 19:41:29 +0530

    Fix error

--- bfhl/src/main/java/com/bajaj/bfhl/controller/BfhlController.java
+++ bfhl/src/main/java/com/bajaj/bfhl/controller/BfhlController.java
@@ -1,6 +1,67 @@
+//////package com.bajaj.bfhl.controller;
+//////
+//////public class BfhlController {
+//////}
+////package com.bajaj.bfhl.controller;
+////
+////import com.bajaj.bfhl.dto.RequestDTO;
+////import com.bajaj.bfhl.dto.ResponseDTO;
+////import com.bajaj.bfhl.service.BfhlService;
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.http.ResponseEntity;
+////import org.springframework.web.bind.annotation.*;
+////
+////@RestController
+////@RequestMapping("/bfhl")
+////public class BfhlController {
+////
+////    @Autowired
+////    private BfhlService bfhlService;
+////
+////    @PostMapping
+////    public ResponseEntity<ResponseDTO> processData(@RequestBody RequestDTO request) {
+////        ResponseDTO response = bfhlService.processData(request);
+////        return ResponseEntity.ok(response);
+////    }
+////}
+////@RestController
+////public class BfhlController {
+////
+////    @GetMapping("/health")
+////    public String health() {
+////        return "API is running";
+////    }
+////
+////    @PostMapping("/bfhl")
+////    public ResponseDTO process(@RequestBody RequestDTO request) {
+////        return service.processData(request);
+////    }
+////}
 //package com.bajaj.bfhl.controller;
 //
+//import com.bajaj.bfhl.dto.RequestDTO;
+//import com.bajaj.bfhl.dto.ResponseDTO;
+//import com.bajaj.bfhl.service.BfhlService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
 //public class BfhlController {
+//
+//    @Autowired
+//    private BfhlService bfhlService;
+//
+//    @GetMapping("/health")
+//    public String health() {
+//        return "API is running";
+//    }
+//
+//    @PostMapping("/bfhl")
+//    public ResponseEntity<ResponseDTO> processData(@RequestBody RequestDTO request) {
+//        ResponseDTO response = bfhlService.processData(request);
+//        return ResponseEntity.ok(response);
+//    }
 //}
 package com.bajaj.bfhl.controller;
 
@@ -12,15 +73,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/bfhl")
 public class BfhlController {
 
     @Autowired
     private BfhlService bfhlService;
 
-    @PostMapping
+    // Health Check API
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("API is running");
+    }
+
+    // Main BFHL API
+    @PostMapping("/bfhl")
     public ResponseEntity<ResponseDTO> processData(@RequestBody RequestDTO request) {
+
         ResponseDTO response = bfhlService.processData(request);
+
         return ResponseEntity.ok(response);
     }
 }
